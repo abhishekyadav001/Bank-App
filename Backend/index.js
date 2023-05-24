45f14c581f945e99e031a9e0200489e9c030f50f
abhishekyadav <yadavabhishek2723@gmail.com>
2023-05-25 01:02:42 +0530

    dotenv error

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const port = process.env.PORT || 8080;
 const express = require("express");
-require("dotenv").config();
-
 const app = express();
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello This is Home Page");
 });
-app.use(express.json());
 
 app.listen(port, () => {
   console.log("listeneing port", port);
--- Backend/route/users.route.js
+++ Backend/route/users.route.js
@@ -1,3 +1,4 @@
 const express = require("express");
 const UserRouter = require(express.Router());
 
+module.exports = { UserRouter };
