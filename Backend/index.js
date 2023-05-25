46d1232f00f7f761655652d9e30f45b6f67005d7
abhishekyadav <yadavabhishek2723@gmail.com>
2023-05-25 13:09:49 +0530

    user router and db connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { UserRoute } = require("./route/users.route");
+const { connection } = require("./config/db");
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", UserRoute);
-app.listen(8080, () => {
+app.listen(8080, async () => {
+  try {
+    await connection();
+    console.log("db connected");
+  } catch (error) {
+    console.log(error.message);
+  }
   console.log("listeneing port", 8080);
 });
--- Backend/route/users.route.js
+++ Backend/route/users.route.js
@@ -13,12 +13,20 @@ UserRoute.get("/", async (req, res) => {
 UserRoute.post("/", async (req, res) => {
   const { username, email, password } = req;
   try {
-    const users = usersModel.create({ username: username, email: email, password: password });
-    res.send({ message: "user_succesfully have been singin" });
+    // const users = usersModel.create({ username: username, email: email, password: password });
+    // res.send({ message: "user_succesfully have been singin" });
+    bcrypte.hash(password, 5, async (err, result) => {
+      const user = new UserModel({ username: username, email: email, password: result });
+      user.save();
+      res.send("sign up successfull");
+    });
   } catch (error) {
     res.send(error.message);
   }
 });
-
+UserRoute.post("/login", async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
 module.exports = { UserRoute };
