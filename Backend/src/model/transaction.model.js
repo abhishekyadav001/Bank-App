d63600059b85d07c58d54c01308ddcd3abcc0ed1
abhishekyadav <yadavabhishek2723@gmail.com>
2023-05-28 15:35:41 +0530

    amount changed

--- Backend/src/model/transaction.model.js
+++ Backend/src/model/transaction.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const transactionSchema = new mongoose.Schema(
   {
     userID: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-    amount: { type: Number, required: true },
+    amount: { type: Number, required: true, min: 1 },
     type: { type: String, enums: ["Deposit", "Withdraw"], required: true },
   },
   { timestamps: true }
--- Backend/src/route/transaction.route.js
+++ Backend/src/route/transaction.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const transactionModel = require("../model/transaction.model");
 const authMiddleware = require("../middleware/auth.middleware");
+const userModel = require("../model/users.model");
 
 const transactionRouter = express.Router();
 transactionRouter.use(authMiddleware);
@@ -17,7 +18,13 @@ transactionRouter.post("/", async (req, res) => {
     const { userID, amount, type } = req.body;
     console.log(amount, type);
     const transaction = await transactionModel.create({ userID, amount, type });
-
+    const currentAmount = await userModel.findById({ _id: userID });
+    const currentBalance = currentAmount.balance;
+    if (type == "Deposit") {
+      const newAmount = currentBalance + amount;
+      const users = await userModel.findByIdAndUpdate({ userID, balance: newAmount });
+      console.log(users);
+    }
     res.status(201).send({ msg: "Transaction Successfull" });
   } catch (error) {
     res.status(401).send(error.message);
